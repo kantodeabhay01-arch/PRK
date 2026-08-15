05ef9dd3bce23c1910f8a652d125274d7df25908
priyanka#FullStack <kantodeabhay01@gmail.com>
2026-08-15 12:52:06 +0530

    LOGIN ACTIVITY

--- app/src/main/java/com/priyanka/myapplication/MainActivity.java
+++ app/src/main/java/com/priyanka/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.priyanka.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,5 +23,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
